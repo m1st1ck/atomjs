4eb1ef48e986837fd3a918dc34915286f72bab80
Veselin Ivanov <veeselinivanov1994@gmail.com>
2023-02-17 17:59:13 +0200

    try to fix rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import typescript from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+
+export default [
+  {
+    input: "src/index.ts",
+    plugins: [
+      typescript(),
+      babel({ extensions: [".ts"], exclude: "node_modules/**" }),
+      terser(),
+    ],
+    output: {
+      dir: "dist",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  },
+  // // UMD
+  {
+    input: "src/index.ts",
+    plugins: [
+      typescript(),
+      babel({ extensions: [".ts"], exclude: "node_modules/**" }),
+      terser(),
+    ],
+    output: {
+      dir: "dist",
+      format: "umd",
+      name: "atomjs",
+      indent: false,
+    },
+  },
+];
